9fe44246450417f8c122c7a2a6d51b407c1a4b65
Jerry <2963025245@qq.com>
2026-02-13 22:01:11 +0800

    登录界面以及跳转

--- frounted/src/views/user/account/login/LoginIndex.vue
+++ frounted/src/views/user/account/login/LoginIndex.vue
@@ -3,7 +3,20 @@
 </script>
 
 <template>
+<div class="flex justify-center mt-30">
+  <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
 
+  <label class="label">Email</label>
+  <input type="email" class="input" placeholder="用户名" />
+
+  <label class="label">Password</label>
+  <input type="password" class="input" placeholder="密码" />
+  <button class="btn btn-neutral mt-4">登录</button>
+    <div class="flex justify-end">
+      <RouterLink :to="{name:'user-account-register-index'}" class="btn btn-neutral mt-4 text-gray-500">注册</RouterLink>
+    </div>
+</fieldset>
+</div>
 </template>
 
 <style scoped>
